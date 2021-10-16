4b379e2cd3dce98a0608f2b28a1eab08dddf002f
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-10-16 20:16:11 +0530

    Make the view show in the Explorer panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-change-count" is now active!');
+class FileChangeCountProvider implements vscode.TreeDataProvider<NodeDetail> {
+	onDidChangeTreeData?: vscode.Event<void | NodeDetail | null | undefined> | undefined;
+	getTreeItem(element: NodeDetail): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+	getChildren(element?: NodeDetail): vscode.ProviderResult<NodeDetail[]> {
+		return [];
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-change-count.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from file-change-count!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+class NodeDetail implements vscode.TreeItem {
+
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	
+	const fileChangeCountProvider = new FileChangeCountProvider();
+	vscode.window.registerTreeDataProvider('fileChangeCount', fileChangeCountProvider);
+}
+
 export function deactivate() {}
